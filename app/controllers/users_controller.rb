2b0f9ff3200b6382df198eb13bfa691837f98df9
Michael Stephens <michael@stephens.tech>
2017-11-27 20:26:28 -0800

    User clean up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -72,6 +72,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
+      params.require(:user).permit(:username, :first_name, :last_name, :email, :password, :password_confirmation)
     end
 end
