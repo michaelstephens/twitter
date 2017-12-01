bb74c0f109803cc9360e3a7fb1c115fd5c438e35
Michael Stephens <michael@stephens.tech>
2017-11-30 21:52:01 -0800

    At mentions

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,22 @@
+include ActionView::Helpers::UrlHelper
+
 class Tweet < ApplicationRecord
   has_many :replies
   validates :content, presence: true
   belongs_to :user
+
+  def at_mention_content
+    content_hash = {}
+    content.split(" ").each_with_index do |c, i|
+      content_hash[i] = c
+    end
+
+    content_hash.select{|k, v| v.include? "@" }.each do |k,v|
+      user = User.where(username: v.gsub("@", "")).first
+      skip if user.blank?
+      content_hash[k] = link_to v, Rails.application.routes.url_helpers.user_path(user.id)
+    end
+
+    content_hash.values.join(" ")
+  end
 end
